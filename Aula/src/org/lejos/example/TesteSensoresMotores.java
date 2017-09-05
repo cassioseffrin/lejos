befd1cf2cca5eab79002f65a39dde6355d2588ae
Cassio Seffrin <cassioseffrin@gmail.com>
2017-09-04 22:26:18 -0300

    Exemplos da aula de lejos 02/09/17

---
+++ Aula/src/org/lejos/example/TesteSensoresMotores.java
@@ -0,0 +1,39 @@
+package org.lejos.example;
+
+ 
+import lejos.nxt.*;
+ 
+public class TesteSensoresMotores {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.out.println("sula sabados");
+                 
+                //LightSensor luz =new LightSensor(SensorPort.S1);
+               UltrasonicSensor us = new UltrasonicSensor(SensorPort.S1);
+                
+                while(!Button.ESCAPE.isPressed()){
+//                    if (luz.readValue()>40){
+//                             gira45Direita();
+//                    }
+                    System.out.println("Distancia "+us.getDistance());
+                    Thread.sleep(500);
+                }
+               
+		Button.waitForPress();
+	}
+        
+        
+        public static void gira45Direita(){
+                Motor.A.forward();
+                Motor.C.backward();
+                try {
+                    Thread.sleep(2000L);
+                } catch (InterruptedException ex) {
+                   
+                }
+                Motor.C.stop();
+                Motor.A.stop();
+            
+        }
+        
+}
---
+++ NXJSegueLinha/src/org/lejos/example/NXJSegueLinha.java
@@ -0,0 +1,24 @@
+package org.lejos.example;
+
+import lejos.nxt.*;
+/**
+ * 
+ * @author cassioseffrin
+ */
+
+public class NXJSegueLinha extends NXBoot {
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Programa para seguir linha");
+        LightSensor luz = new LightSensor(SensorPort.S1);
+        while (!Button.ESCAPE.isPressed()) {
+            if (luz.readValue() > 40) {
+                avanca(100);
+                esquerda(50);
+            } else {
+                avanca(10);
+                direita(10);
+            }
+        }
+    }
+}
